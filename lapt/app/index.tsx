f7eb6fbd53209dff60fe9fe27471de41ccbdd63c
Danlyn Medou <danlynmedou@Dans-Macbook-Pro.local>
2024-09-26 14:42:21 +0200

    completly changed the structure, restart

---
+++ lapt/app/index.tsx
@@ -0,0 +1,23 @@
+//index.tsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to My App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
