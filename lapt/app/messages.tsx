374087e5ea324859134dbadccd2704de840bd17f
Danlyn Medou <danlynmedou@Dans-Macbook-Pro.local>
2024-09-28 14:50:38 +0200

    messages szection fully done

--- lapt/app/messages.tsx
+++ lapt/app/messages.tsx
@@ -9,6 +9,82 @@ const MessagesScreen = () => {
 
   const tabs = ['Traffic', 'Perturbation', 'News'];
 
+  const newsItems = [
+    {
+      date: { day: '23', month: 'avril' },
+      title: 'Travis Scott !',
+      subtitle: 'Le millionaire Midas achète tweeter',
+      headline: 'Travis Scott en concert à Sweaty Sands',
+      content: `Des entrelacs de lumières dignes des meilleurs shows laser, des changements de décors, un public en folie : un concert historique a bien eu lieu sur notre île. Le 23 avril, le rappeur Travis Scott s'est produit en live, interprétant un medley de ses tubes dans des décors de parcs d'attractions en flammes, de mondes subaquatiques et de cieux étoilés.`,
+      color: '#FF69B4'
+    },
+    {
+      date: { day: '25', month: 'avril' },
+      title: 'Nouveau Bus !',
+      subtitle: 'Une ligne express vers le centre-ville',
+      headline: 'Lancement d\'une nouvelle ligne de bus express',
+      content: 'La ville annonce le lancement d\'une nouvelle ligne de bus express reliant les quartiers périphériques au centre-ville. Ce service vise à réduire le temps de trajet et à encourager l\'utilisation des transports en commun.',
+      color: '#4CAF50'
+    },
+    {
+      date: { day: '27', month: 'avril' },
+      title: 'Fête de la Ville',
+      subtitle: 'Célébrons ensemble ce week-end',
+      headline: 'Grande fête de la ville ce week-end',
+      content: 'Ne manquez pas la grande fête de la ville ce week-end ! Au programme : concerts, animations de rue, feux d\'artifice et bien d\'autres surprises. Venez nombreux pour célébrer notre belle cité !',
+      color: '#FFA500'
+    }
+  ];
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case 'Traffic':
+        return (
+          <>
+            <Text style={styles.sectionTitle}>INFORMATION TRAFIC</Text>
+            <View style={styles.messageItem}>
+              <Ionicons name="information-circle-outline" size={24} color="white" style={styles.icon} />
+              <Text style={styles.messageText}>Vous pouvez y consulter des informations sur le trafic.</Text>
+            </View>
+          </>
+        );
+      case 'Perturbation':
+        return (
+          <View style={styles.messageItem}>
+            <Ionicons name="information-circle-outline" size={24} color="#8e8e93" style={styles.icon} />
+            <View>
+              <Text style={styles.messageText}>Ici vous pouvez vous renseigner sur les incidents.</Text>
+              <Text style={styles.messageSubtext}>Et les incidents majeurs dans votre région.</Text>
+            </View>
+          </View>
+        );
+        case 'News':
+            return (
+                <>
+                <Text style={styles.sectionTitle}>NEWS</Text>
+                {newsItems.map((item, index) => (
+                    <View key={index} style={styles.newsContainer}>
+                    <View style={[styles.newsCard, { backgroundColor: item.color }]}>
+                        <View style={styles.dateCircle}>
+                        <Text style={styles.dateText}>{item.date.day}</Text>
+                        <Text style={styles.dateMonth}>{item.date.month}</Text>
+                        </View>
+                        <Text style={styles.newsTitle}>{item.title}</Text>
+                        <Text style={styles.newsSubtitle}>{item.subtitle}</Text>
+                    </View>
+                    <View style={styles.newsContentContainer}>
+                        <Text style={styles.newsHeadline}>{item.headline}</Text>
+                        <Text style={styles.newsContent}>{item.content}</Text>
+                    </View>
+                    </View>
+                ))}
+                </>
+            );
+      default:
+        return null;
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Messages</Text>
@@ -24,11 +100,7 @@ const MessagesScreen = () => {
         ))}
       </View>
       <ScrollView style={styles.messageContainer}>
-        <Text style={styles.sectionTitle}>INFORMATION TRAFIC</Text>
-        <View style={styles.messageItem}>
-          <Ionicons name="information-circle-outline" size={24} color="white" style={styles.icon} />
-          <Text style={styles.messageText}>Vous pouvez y consulter des informations sur le trafic.</Text>
-        </View>
+        {renderTabContent()}
       </ScrollView>
       <Navbar />
     </SafeAreaView>
@@ -90,6 +162,68 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 14,
     flex: 1,
+  },
+  messageSubtext: {
+    color: '#8e8e93',
+    fontSize: 12,
+    marginTop: 5,
+  },
+  newsContainer: {
+    backgroundColor: '#2c2c2e',
+    borderRadius: 10,
+    marginBottom: 20,
+    overflow: 'hidden',
+  },
+  newsCard: {
+    backgroundColor: '#FF69B4',
+    borderRadius: 10,
+    padding: 100,
+    marginBottom: 20,
+  },
+  dateCircle: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    backgroundColor: '#FF4500',
+    borderRadius: 20,
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dateText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  dateMonth: {
+    color: 'white',
+    fontSize: 10,
+  },
+  newsTitle: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  newsSubtitle: {
+    color: 'white',
+    fontSize: 16,
+  },
+  newsContentContainer: {
+    padding: 20,
+  },
+  newsHeadline: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  newsContent: {
+    color: '#8e8e93',
+    fontSize: 14,
+    lineHeight: 20,
+    marginBottom: 25,
   },
 });
 
---
+++ lapt/app/more.tsx
@@ -0,0 +1,141 @@
+// /Users/danlynmedou/Desktop/smartCity/lapt/app/more.tsx
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Switch } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import Navbar from '@/components/Navbar';
+
+const MoreScreen = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const toggleDarkMode = () => setIsDarkMode(previousState => !previousState);
+
+  const renderListItem = (title: string, icon: string, showArrow: boolean = true) => (
+    <TouchableOpacity style={styles.listItem}>
+      <Ionicons name={icon as any} size={24} color="#8e8e93" />
+      <Text style={styles.listItemText}>{title}</Text>
+      {showArrow && <Ionicons name="chevron-forward" size={24} color="#8e8e93" />}
+    </TouchableOpacity>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Plus</Text>
+      
+      <TouchableOpacity style={styles.accountCard}>
+        <View style={styles.accountIconContainer}>
+          <Ionicons name="person" size={24} color="white" />
+        </View>
+        <View style={styles.accountInfo}>
+          <Text style={styles.accountTitle}>Mon compte</Text>
+          <Text style={styles.accountSubtitle}>Connectez-vous ou créez un compte pour bénéficier de nos avantages</Text>
+        </View>
+        <Ionicons name="chevron-forward" size={24} color="#8e8e93" />
+      </TouchableOpacity>
+
+      <Text style={styles.sectionTitle}>Mes Infos</Text>
+      {renderListItem('Numéro de téléphone', 'call-outline')}
+      {renderListItem('Options de Paiement', 'card-outline')}
+      {renderListItem('Coupons', 'pricetag-outline')}
+      {renderListItem('Reçus et historique des achats', 'receipt-outline')}
+
+      <View style={styles.darkModeContainer}>
+        <Text style={styles.darkModeText}>Mode Claire</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isDarkMode ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleDarkMode}
+          value={isDarkMode}
+        />
+      </View>
+
+      <Text style={styles.sectionTitle}>Paramètres</Text>
+      {renderListItem('Retirer des informations', 'information-circle-outline')}
+      {renderListItem('Info trafic et contrôle', 'bus-outline')}
+      {renderListItem('Language', 'language-outline')}
+
+      <Navbar />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1c1c1e',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: 'white',
+    padding: 20,
+  },
+  accountCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#2c2c2e',
+    borderRadius: 10,
+    padding: 15,
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  accountIconContainer: {
+    backgroundColor: '#0a84ff',
+    borderRadius: 20,
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  accountInfo: {
+    flex: 1,
+  },
+  accountTitle: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  accountSubtitle: {
+    color: '#8e8e93',
+    fontSize: 12,
+  },
+  sectionTitle: {
+    color: '#8e8e93',
+    fontSize: 12,
+    fontWeight: 'bold',
+    marginLeft: 20,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#2c2c2e',
+  },
+  listItemText: {
+    color: 'white',
+    fontSize: 16,
+    marginLeft: 15,
+    flex: 1,
+  },
+  darkModeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#2c2c2e',
+  },
+  darkModeText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default MoreScreen;
+
